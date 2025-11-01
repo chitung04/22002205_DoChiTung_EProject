31cb4721e5f555f40563834332a038477243f003
Đỗ Chí Tùng <chitung568@gmail.com>
2025-11-01 09:54:56 +0700

    CI CD

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -110,24 +110,27 @@ class ProductController {
     }
   }
   async getProductById(req, res, next) {
-    try {
+    try{
       const token = req.headers.authorization;
-      if (!token) {
-        return res.status(401).json({ message: "Unauthorized" });
+      if(!token){
+      return res.status(401).json({message:"Unauthorized"});
       }
+    
 
+
+    
       const { id } = req.params;
       const product = await Product.findById(id);
-      if (!product) {
-        return res.status(404).json({ message: "Product not found" });
-      }
+    if (!product){
+      return res.status(404).json({message:"Product not found"});
 
-      res.status(200).json(product); // ✅ sửa chỗ này
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Server error" });
     }
-  }
+      res.status(200).json(product); 
+    }catch(error){
+      console.error(error);
+      return res.status(500).json({message:"Server errror"});
+    }
+}
 }
 
 module.exports = ProductController;
